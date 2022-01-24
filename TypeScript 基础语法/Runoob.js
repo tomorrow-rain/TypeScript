cb6fbda8c6b1cdf752a9033bf13bf1308debf8ef
youngmanymy <532442030@qq.com>
2022-01-25 01:25:15 +0800

    first commit

---
+++ TypeScript 基础语法/Runoob.js
@@ -0,0 +1,12 @@
+var hello = "Hello World!";
+console.log(hello);
+var Site = /** @class */ (function () {
+    function Site() {
+    }
+    Site.prototype.name = function () {
+        console.log("Runoob");
+    };
+    return Site;
+}());
+var obj = new Site();
+obj.name();
---
+++ TypeScript 基础语法/Runoob.ts
@@ -0,0 +1,10 @@
+const hello : string = "Hello World!";
+console.log(hello);
+
+class Site { 
+    name():void { 
+       console.log("Runoob") 
+    } 
+ } 
+ var obj = new Site(); 
+ obj.name();
